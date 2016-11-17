34a6815483263a2adffb225171d7ccba3389e70b
Maya Friedrichs <mail@hyakosm.net>
2016-11-17 19:52:51 +0100

    Nouveautés UI et avancement socket

--- app/src/main/java/com/petut/thobbyo/petut/Acceuil.java
+++ app/src/main/java/com/petut/thobbyo/petut/Acceuil.java
@@ -1,5 +1,6 @@
 package com.petut.thobbyo.petut;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -19,38 +20,82 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.SocketAddress;
 
 public class Acceuil extends AppCompatActivity {
+    public class Chaussette extends AsyncTask<String, String, String> {
+        TextView tcp_text = (TextView) findViewById(R.id.tcp_text);
+
+        @Override
+        protected String doInBackground(String... arg) {
+            try {
+                Socket chaussette = new Socket("thgros.fr", 4242);
+                InputStream in = chaussette.getInputStream();
+                OutputStream out = chaussette.getOutputStream();
+                out.write(arg[0].getBytes());
+
+                int nbytes;
+                String reponse = "";
+                byte buf[] = new byte[512];
+
+                /*while ((nbytes = in.read(buf)) != -1) {
+                    reponse += new String(buf).substring(0, nbytes);
+                }*/
+
+                chaussette.close();
+
+                return arg[0];
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "ERROR";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String reponse) {
+            tcp_text.setText(tcp_text.getText().toString() + reponse + "\n");
+        }
+    }
+
     EditText tcp_req;
     TextView tcp_text;
     Button tcp_envoi;
-    Chaussette chau;
+    Button plan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acceuil);
-        final EditText tcp_req = (EditText) findViewById(R.id.tcp_req);
-        final TextView tcp_text = (TextView) findViewById(R.id.tcp_text);
-        Button tcp_envoi = (Button) findViewById(R.id.tcp_envoi);
-        final Chaussette chau = new Chaussette();
+
+        tcp_req = (EditText) findViewById(R.id.tcp_req);
+        tcp_text = (TextView) findViewById(R.id.tcp_text);
+        tcp_envoi = (Button) findViewById(R.id.tcp_envoi);
+        plan = (Button) findViewById(R.id.plan);
 
         tcp_envoi.setOnClickListener(new View.OnClickListener() {
             @Override
+            //tcp_req.getText().toString()
             public void onClick(View view) {
-                String reponse = chau.Send(tcp_req.getText().toString());
-                tcp_text.setText(tcp_text.getText().toString() + "\n" + reponse);
+                new Chaussette().execute("select * from test");
             }
         });
-    }
 
-    public void changeView(View view) {
-        Intent intent = new Intent(Acceuil.this, MapsMenu.class);
-        startActivity(intent);
+        plan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Acceuil.this, MapsMenu.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(intent);
+            }
+        });
     }
 
     /**
