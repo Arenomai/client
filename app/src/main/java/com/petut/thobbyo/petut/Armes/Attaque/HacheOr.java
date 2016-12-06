3e1574488b7248fece6f4bb264df5a8557f2ab11
Maya Friedrichs <mail@hyakosm.net>
2016-12-06 11:43:07 +0100

    Nouveaux objets d'attaque : haches fer et or

---
+++ app/src/main/java/com/petut/thobbyo/petut/Armes/Attaque/HacheOr.java
@@ -0,0 +1,14 @@
+package com.petut.thobbyo.petut.Armes.Attaque;
+
+import com.petut.thobbyo.petut.Armes.Arme;
+import com.petut.thobbyo.petut.R;
+
+/**
+ * Created by hyakosm on 06/12/16.
+ */
+
+public class HacheOr extends Arme {
+    public HacheOr() {
+        super("Hache en or", 25, true, R.drawable.attaque_hache_or);
+    }
+}
