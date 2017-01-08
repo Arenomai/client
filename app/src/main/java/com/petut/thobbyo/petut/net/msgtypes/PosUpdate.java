8161e4290af4d306d7f315de77d8f82138a2aa4f
Celeste Wouters <elementw@openmailbox.org>
2017-01-08 17:28:16 +0100

    net: implé de base des messages d'auth & màj pos

---
+++ app/src/main/java/com/petut/thobbyo/petut/net/msgtypes/PosUpdate.java
@@ -0,0 +1,32 @@
+package com.petut.thobbyo.petut.net.msgtypes;
+
+import android.location.Location;
+
+import com.petut.thobbyo.petut.net.InMessage;
+import com.petut.thobbyo.petut.net.OutMessage;
+import com.petut.thobbyo.petut.net.Serializable;
+
+public class PosUpdate {
+
+    public static abstract class Subtype {
+        public static final byte
+                Notify = 0;
+    }
+
+    public static class Notify implements Serializable {
+        public Location loc;
+
+        @Override
+        public void writeToMsg(OutMessage msg) {
+            msg.writeDouble(loc.getLatitude());
+            msg.writeDouble(loc.getLongitude());
+        }
+
+        @Override
+        public void readFromMsg(InMessage msg) {
+            loc = new Location("GameServer");
+            loc.setLongitude(msg.readDouble());
+            loc.setLatitude(msg.readDouble());
+        }
+    }
+}
