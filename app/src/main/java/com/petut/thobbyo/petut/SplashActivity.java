2abd3d1f238460a47cea5c016514ce6f91745217
Thomas GROS <thomas.naxx@hotmail.fr>
2017-01-09 23:00:44 +0100

    Test GPS & sortie obligatoire

--- app/src/main/java/com/petut/thobbyo/petut/SplashActivity.java
+++ app/src/main/java/com/petut/thobbyo/petut/SplashActivity.java
@@ -1,6 +1,9 @@
 package com.petut.thobbyo.petut;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -8,9 +11,38 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }else {
+            showGPSDisabledAlertToUser();
+        }
 
-        Intent intent = new Intent(this, CheckGPSActivity.class);
-        startActivity(intent);
-        finish();
+    }
+
+    private void showGPSDisabledAlertToUser(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("Le GPS n'a pas été activé sur votre appareil, vous devez l'activer maintenant pour pouvoir jouer au jeu. Voulez-vous l'activer maintenant ?")
+                .setCancelable(false)
+                .setPositiveButton("Aller à la page d'activation du GPS",
+                        new DialogInterface.OnClickListener(){
+                            public void onClick(DialogInterface dialog, int id){
+                                Intent callGPSSettingIntent = new Intent(
+                                        android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                                startActivity(callGPSSettingIntent);
+                                finish();
+                            }
+                        });
+        alertDialogBuilder.setNegativeButton("Quitter le jeu",
+                new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog, int id){
+                        dialog.cancel();
+                        finish();
+                    }
+                });
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
     }
 }
