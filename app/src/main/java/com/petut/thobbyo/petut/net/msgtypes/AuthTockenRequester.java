9439ae3104101146ba3ff4badaab8918f0eb8726
Dorian Wouters <elementw@openmailbox.org>
2017-01-14 20:10:23 +0100

    Plus d'async

--- app/src/main/java/com/petut/thobbyo/petut/net/msgtypes/AuthTockenRequester.java
+++ app/src/main/java/com/petut/thobbyo/petut/net/msgtypes/AuthTockenRequester.java
@@ -1,11 +1,7 @@
 package com.petut.thobbyo.petut.net.msgtypes;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.AsyncTask;
 
-import com.petut.thobbyo.petut.Application;
 import com.petut.thobbyo.petut.net.Connection;
 import com.petut.thobbyo.petut.net.InMessage;
 import com.petut.thobbyo.petut.net.MessageType;
@@ -13,46 +9,36 @@ import com.petut.thobbyo.petut.net.OutMessage;
 
 import java.io.IOException;
 
-import static com.petut.thobbyo.petut.Application.PREF_DEFAULT;
 
-/**
- * Created by Krevar on 09/01/2017.
- */
+public class AuthTockenRequester extends AsyncTask<String, Void, Integer> {
 
-public class AuthTockenRequester extends AsyncTask<String, String, String> {
+    private Connection co;
 
-    private static Context context;
-
-    public AuthTockenRequester(Context ctx) {
-        context = ctx;
+    public AuthTockenRequester(Connection co) {
+        this.co = co;
     }
-    @Override
-    protected String doInBackground(String... params) {
-        final Connection co = new Connection();
-        final Context ctx;
-        final SharedPreferences sp = context.getSharedPreferences(PREF_DEFAULT, 0);
-        final SharedPreferences.Editor spe = sp.edit();
 
+    @Override
+    protected Integer doInBackground(String... params) {
         try {
-            co.connect(sp.getString("serverAddr", "thgros.fr"), sp.getInt("serverPort", 4242));
             OutMessage omsg = new OutMessage(MessageType.Auth, Auth.Subtype.Request);
             omsg.writeString(params[0]);
             omsg.writeString(params[1]);
             co.write(omsg);
             InMessage inmsg = co.read();
+
+            int token;
             if (inmsg.getSubtype() == Auth.Subtype.Denied) {
-                spe.putInt("token", -2);
+                token = -2;
             } else {
-                int token = inmsg.readI32();
-                spe.putInt("token", token);
+                token = inmsg.readI32();
             }
-            spe.apply();
             co.close();
+            return token;
         } catch (IOException e) {
             e.printStackTrace();
-            return "ERROR";
         }
-        return "";
+        return null;
     }
 }
 
