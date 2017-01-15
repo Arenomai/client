9c91bc785552dddb37d4ae81dfa8933800417af8
Thomas LECOUFFE <Thomas LECOUFFE>
2017-01-15 18:43:07 +0100

    IA fonctionnel (reste peu être quelque BUG)

--- app/src/main/java/com/petut/thobbyo/petut/jeuDeCarte/Joueurs.java
+++ app/src/main/java/com/petut/thobbyo/petut/jeuDeCarte/Joueurs.java
@@ -1,5 +1,12 @@
 package com.petut.thobbyo.petut.jeuDeCarte;
 
+import android.content.Context;
+
+import com.petut.thobbyo.petut.R;
+
+import java.util.List;
+import java.util.Random;
+
 /**
  * Created by Thobbyo on 14/01/2017.
  */
@@ -32,4 +39,30 @@ public class Joueurs {
         pv -= dega;
     }
 
+    private static int randomInteger(int aStart, int aEnd, Random aRandom){
+        long range = (long)aEnd - (long)aStart + 1;
+        long fraction = (long)(range * aRandom.nextDouble());
+        int randomNumber =  (int)(fraction + aStart);
+        return randomNumber;
+    }
+
+    public void IA(List<Monstre> monstre, List<Defense> defense, Context contexte){
+        if(position == 0){
+            Random randomGenerator = new Random();
+            Random ran = new Random();
+            int randomInt = randomGenerator.nextInt(2);
+
+            if(randomInt == 0) {
+                int randY = randomInteger(0, 1, ran);
+                int randX = randomInteger(0, 4, ran);
+                monstre.add(new Monstre(1, 1, randX, randY, 1, 2, 2, "Monstre pas bô", new Image(contexte, R.mipmap.monstre_sourire), 0));
+            }
+
+            if(randomInt == 1){
+                int randY = randomInteger(2, 3, ran);
+                int randX = randomInteger(0, 4, ran);
+                defense.add(new Defense(1, 1, randX, randY, 4, 0, "MÛRE", new Image(contexte, R.mipmap.bleu_mur_icone_128), 0));
+            }
+        }
+    }
 }
