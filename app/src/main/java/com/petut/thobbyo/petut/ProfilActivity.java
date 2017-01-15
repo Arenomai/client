352ff8a63f81c649286532246de7f5ab9601f553
Thomas GROS <thomas.naxx@hotmail.fr>
2017-01-15 17:53:27 +0100

    Fix déconnexion

--- app/src/main/java/com/petut/thobbyo/petut/ProfilActivity.java
+++ app/src/main/java/com/petut/thobbyo/petut/ProfilActivity.java
@@ -1,7 +1,10 @@
 package com.petut.thobbyo.petut;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v4.content.IntentCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -63,7 +66,14 @@ public class ProfilActivity extends AppCompatActivity {
                         int duration = Toast.LENGTH_SHORT;
                         Toast toast = Toast.makeText(getApplicationContext(), "Déconnexion", duration);
                         toast.show();
-                        System.exit(0);
+                        Intent intent = new Intent(getApplicationContext(), SplashActivity.class);
+                        ComponentName cn = intent.getComponent();
+                        Intent mainIntent = IntentCompat.makeRestartActivityTask(cn);
+                        mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                        mainIntent.putExtra("close", true);
+                        startActivity(mainIntent);
+                        finish();
 
 
                     }
