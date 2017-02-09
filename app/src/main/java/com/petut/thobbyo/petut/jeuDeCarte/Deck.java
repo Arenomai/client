270a138d98d97c8679550e2d0ee8fcfeb4c60a04
Thomas LECOUFFE <Thomas LECOUFFE>
2017-02-09 16:58:26 +0100

    Ajoue de la class Deck

---
+++ app/src/main/java/com/petut/thobbyo/petut/jeuDeCarte/Deck.java
@@ -0,0 +1,17 @@
+package com.petut.thobbyo.petut.jeuDeCarte;
+
+import java.util.List;
+
+/**
+ * Created by Thobbyo on 09/02/2017.
+ */
+
+public class Deck {
+
+    List<Carte> mesCartes;
+
+    public Deck(List<Carte> lesMienne){
+        mesCartes = lesMienne;
+    }
+
+}
